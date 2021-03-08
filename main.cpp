415aac528b3fb04023037286c01097be858b7b3b
mila <andjelaniketic109@gmail.com>
2021-03-08 14:21:11 +0100

    dodati VAO i VBO

--- main.cpp
+++ main.cpp
@@ -49,6 +49,34 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    // OBJEKAT EBO //////////////////////
+
+    float vertices[] = {
+            0.0f, 0.5f, 0.0f, // gore
+            0.5f, -0.5f, 0.0f,  // desno
+            -0.5f, -0.5f, 0.0f  // levo
+    };
+
+    unsigned int VBO; //vertex buffer object, iz RAM-a ucitava podatke na GPU
+    unsigned int VAO; //vertex arr obj, govori openglu sta znace objekti u baferu
+
+    glGenBuffers(1, &VBO); // generise bafer
+    glGenVertexArrays(1, &VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //aktivira bafer
+    glBindVertexArray(VAO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+
+
     //petlja renderovanja
     while(!glfwWindowShouldClose(window)) {
 
